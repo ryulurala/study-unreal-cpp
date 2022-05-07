3bcbba2528bfb74da41670a97a4b892aa647a20a
ryulurala <ryulurala@gmail.com>
2022-05-07 17:42:36 +0900

    [Project] Animation Basic

---
+++ project/Test/Source/Test/MyAnimInstance.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAnimInstance.h"
+
+void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);		// GENERATE_BODY()에 Super 키워드 존재
+
+	APawn* MyPawn = TryGetPawnOwner();
+	if (IsValid(MyPawn))
+	{
+		Speed = MyPawn->GetVelocity().Size();
+	}
+}
---
+++ project/Test/Source/Test/MyAnimInstance.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_API UMyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	// Native가 붙으면 인게임 뿐만 아니라 에디터 내에서도 호출
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	float Speed;
+
+};
